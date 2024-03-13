642e48b6ddd7abddb0b21bb561a7d375cc1c05c6
Horad <iranradmard1@gmail.com>
2024-03-14 03:27:57 +0330

    In progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import ListGroup from "./components/ListGroup";
 import Message from "./Message";
 
 function App() {
-  
-  return <div><Message></Message><ListGroup></ListGroup></div>
-
+  return (
+    <div>
+      <Message></Message>
+      <ListGroup></ListGroup>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,26 @@
+// import { Fragment } from "react/jsx-runtime";
+
 function ListGroup() {
-    return <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-    </ul>;
+  const items = ["mamad", "ali", "jafar", "asqar"];
+  if (items.length === 5) {
+    return null;
+  }
+  return (
+    <>
+      <h1>Hellooooo</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={() => alert("Clicked on " + item + " " + index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
